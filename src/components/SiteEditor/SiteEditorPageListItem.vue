b79aa97496a058b7d840126ee78e336429a95b97
ulikemywa1 <calium@outlook.com>
2024-06-25 17:52:30 +0300

    feat: implement responsive UI

--- src/components/SiteEditor/SiteEditorPageListItem.vue
+++ src/components/SiteEditor/SiteEditorPageListItem.vue
@@ -1,7 +1,9 @@
 <template>
   <li class="page-list__item">
     <div class="page__badge bg-1"></div>
-    <span class="page__title">{{ title }}</span>
+    <div class="page__title">
+      <span>{{ title }}</span>
+    </div>
     <div class="button-wrapper">
       <button class="button page-setting-btn">Settings</button>
       <button class="button danger-btn page-remove-btn" @click="killMe">Delete</button>
