2d068ab3887bb5f466f4451bd225390f7e18e546
ulikemywa1 <calium@outlook.com>
2024-06-26 12:02:47 +0300

    feat: show page description and page url

--- src/components/SiteEditor/SiteEditorPageListItem.vue
+++ src/components/SiteEditor/SiteEditorPageListItem.vue
@@ -7,9 +7,19 @@
       tabindex="0"
       @click="goToPageEdit"
     ></div>
-    <div class="page__title">
-      <span @click="goToPageEdit" class="clickable">{{ title }}</span>
+    <div class="page__info-wrapper">
+      <div class="page__title">
+        <span @click="goToPageEdit" class="clickable">{{ title }}</span>
+      </div>
+      <div class="page__descr">
+        <span>Page description: </span
+        >{{ this.sitesStore.getPageDescr(this.belongTo, this.pageID) }}
+      </div>
+      <div class="page__url">
+        <span>Page address: </span>{{ this.sitesStore.getPageURL(this.belongTo, this.pageID) }}
+      </div>
     </div>
+
     <div class="button-wrapper">
       <button class="button page-setting-btn" @click="openPageEditor">Settings</button>
       <button class="button danger-btn page-remove-btn" @click="killMe">Delete</button>
